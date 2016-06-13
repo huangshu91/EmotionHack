d3f0da7c0349849bb27eefad5830178bfedf64f4
vivekra <vivekra@VIVEKRA-LT>
2016-06-12 19:37:02 -0700

    Barebone DataLayer. Note: nothing is impletemented yet. It's enough to unblock work on other components.

---
+++ Orchestrator/DataStore/IDataLayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStore
+{
+    /// <summary>
+    /// Defines methods to query, insert, and modify data in the data store
+    /// </summary>
+    public interface IDataLayer
+    {
+        /// <summary>
+        /// Query's data from the underlying data store
+        /// </summary>
+        /// <typeparam name="T">The Type of the data to return back</typeparam>
+        /// <param name="query">The query to send to the store, to get the desired data</param>
+        /// <returns>Data from the store, deserialized to type T</returns>
+        T QueryData<T>(string query);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="insertQuery"></param>
+        /// <returns></returns>
+        bool InsertSingleDataInputThroughQuery(string insertQuery);
+    }
+}
---
+++ Orchestrator/DataStore/SQLDataLayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStore
+{
+    /// <summary>
+    /// Implementation of the Data layer for SQL databases
+    /// </summary>
+    class SQLDataLayer : IDataLayer
+    {
+        /// <summary>
+        /// Sets up the connection to the SQL database
+        /// </summary>
+        /// <param name="databaseConnection"></param>
+        public SQLDataLayer(string databaseConnection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertSingleDataInputThroughQuery(string insertQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T QueryData<T>(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
