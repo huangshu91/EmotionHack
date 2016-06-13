95c504996f396493f771c234ab2f0690efed1c31
vivekra <vivekra@VIVEKRA-LT>
2016-06-12 20:45:48 -0700

    Adding Windows Media player to the form.

--- Orchestrator/Orchestrator/Program.cs
+++ Orchestrator/Orchestrator/Program.cs
@@ -19,7 +19,7 @@ namespace Orchestrator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new VideoPlayerForm());
 
             //EmotionClient client = new EmotionClient();
 
