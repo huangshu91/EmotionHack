f9c0b44e5a594c859c6f96cecb7d471b7916b758
shuhuan <shuhuan@microsoft.com>
2016-06-23 16:35:48 -0700

    add test project

---
+++ Orchestrator/EmotionalTest/EmotionalClientTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmotionalTest
+{
+    [TestClass]
+    public class EmotionalClientTest
+    {
+        string TestInput = @"/TestResources/happybaby_lowres.jpg";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Orchestrator/EmotionalTest/EmotionalDbTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmotionalTest
+{
+    [TestClass]
+    public class EmotionalDbTest
+    {
+        private string _connection = @"Server=tcp:emotiondb.database.windows.net,1433;Database=EmotionHackPPE;User ID=emotionhack@emotiondb;Password=@NYTH!NG123;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
