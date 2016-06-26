95a5ca664326f553a636689941fc8a307aa9fdba
Vivek Ravindran (VIVEKRA) <vivekra@microsoft.com>
2016-06-25 18:03:01 -0700

    WPF Media player + Emo test ref

---
+++ Orchestrator/WPFMediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool _playState;
+        public MainWindow()
+        {
+            _playState = false;
+            InitializeComponent();
+        }
+
+        private void mediaElement_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.O:
+                    OpenFile();
+                    break;
+                case Key.P:
+                    PlayPause();
+                    break;
+                case Key.S:
+                    Stop();
+                    break;
+            }
+        }
+
+        private void Stop()
+        {
+            mediaElement.Stop();
+            _playState = false;
+        }
+
+        private void PlayPause()
+        {
+            if (!_playState) mediaElement.Play();
+            else mediaElement.Pause();
+            _playState = !_playState;
+        }
+
+        private void OpenFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.AddExtension = true;
+            ofd.DefaultExt = "*.*";
+            ofd.Filter = "Media (*.*)|*.*";
+            ofd.ShowDialog();
+            mediaElement.Source = new Uri(ofd.FileName);
+        }
+    }
+}
