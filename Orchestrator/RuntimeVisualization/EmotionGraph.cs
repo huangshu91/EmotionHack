13e0368e55e774bab66ff86fadd437c8bfdd310d
yang liu <yang liu@YANGLIU-PC>
2016-07-23 19:42:04 -0700

    Run time visulaization updates

--- Orchestrator/RuntimeVisualization/EmotionGraph.cs
+++ Orchestrator/RuntimeVisualization/EmotionGraph.cs
@@ -10,26 +10,30 @@
 
     public class EmotionGraph
     {
+        private int _displaySpan = 10;
         public EmotionGraph()
         {
             this.MyModel = new PlotModel { Title = "Example 1" };
+            //this.MyModel.PlotAreaBackground = BackgroundBaseColor.ToOxyColor();
+            this.MyModel.Background = OxyColors.White;
 
             Line = new LineSeries
             {
                 StrokeThickness = 2,
                 CanTrackerInterpolatePoints = false,
                 Title = "Value",
-                Smooth = false
+                Smooth = true
             };
 
             this.MyModel.Series.Add(Line);
             this.MyModel.Axes.Clear();
             var t = this.MyModel.Axes.Count;
 
-            this.MyModel.IsLegendVisible = false;
+            //this.MyModel.IsLegendVisible = false;
+            this.MyModel.IsLegendVisible = true;
 
             Timer myTimer = new Timer();
-            //myTimer.Elapsed += new ElapsedEventHandler(UpdateModel);
+            myTimer.Elapsed += new ElapsedEventHandler(UpdateModel);
             myTimer.Interval = 1000; // 1000 ms is one second
             myTimer.Start();
         }
@@ -37,12 +41,14 @@
         public void UpdateModel(object source, ElapsedEventArgs e)
         {
             //MyModel.Series.Clear();
-            //Line.Points.Add(new DataPoint(testx++, testy));
-            //testy *= -1;
-
-            //MyModel.InvalidatePlot(true);
+            Line.Points.Add(new DataPoint(testx++, testy));
+            testy *= -1;
+            if (testx >= _displaySpan) {
+                Line.Points.RemoveAt(0);
+            }
+            MyModel.InvalidatePlot(true);
         }
-        
+
         public void UpdateScore(EmotionScore emo)
         {
             //display the score in some way            
