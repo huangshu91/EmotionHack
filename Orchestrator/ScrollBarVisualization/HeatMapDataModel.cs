f16f237fef8aefdbf8581ba34f24bec5dfe10a74
vivekra <vivekra@VIVEKRA-LT>
2016-07-25 22:40:34 -0700

    Adding Scroll bar visualization stub project and stuff

---
+++ Orchestrator/ScrollBarVisualization/HeatMapDataModel.cs
@@ -0,0 +1,19 @@
+﻿using Emotional.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrollBarVisualization
+{
+    class HeatMapDataModel
+    {
+        public HeatMapDataModel()
+        {
+        }
+
+        public void UpdateScore(EmotionScore emo)
+        {
+        }
+}
---
+++ Orchestrator/ScrollBarVisualization/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Emotional.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScrollBarVisualization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private HeatMapDataModel Model;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Model = (HeatMapDataModel)this.DataContext;
+        }
+
+        public void UpdateData(EmotionScore emo)
+        {
+            Model.UpdateScore(emo);
+        }
+    }
+}
