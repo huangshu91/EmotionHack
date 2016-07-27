780897de085d600c003dcdea0f3971379f464948
vivekrav <vivek256@gmail.com>
2016-07-26 20:02:45 -0700

    Updating Pie chart with emo scores

--- Orchestrator/ScrollBarVisualization/PieViewModel.cs
+++ Orchestrator/ScrollBarVisualization/PieViewModel.cs
@@ -12,7 +12,7 @@
         {
             MyModel = new PlotModel();
 
-            dynamic seriesP1 = new PieSeries {
+            var seriesP1 = new PieSeries {
                 StrokeThickness = 2.0,
                 InsideLabelPosition = 0.8,
                 AngleSpan = 360,
@@ -22,18 +22,45 @@
                 Stroke = OxyColors.Gray
             };
 
-            seriesP1.Slices.Add(new PieSlice("Africa", 1030) { IsExploded = false, Fill = OxyColors.PaleVioletRed,  });
-            seriesP1.Slices.Add(new PieSlice("Americas", 929) { IsExploded = true });
-            seriesP1.Slices.Add(new PieSlice("Asia", 4157) { IsExploded = true });
-            seriesP1.Slices.Add(new PieSlice("Europe", 739) { IsExploded = true });
-            seriesP1.Slices.Add(new PieSlice("Oceania", 300) { IsExploded = true });
+            seriesP1.Slices.Add(new PieSlice("No Data Available", 1) { IsExploded = false, Fill = OxyColors.Gray,  });
 
             MyModel.Series.Add(seriesP1);
         }
 
         public void UpdateScore(EmotionScore emo)
         {
-            //Model.UpdateScore(emo);
+            if (MyModel.Series.Count > 0) MyModel.Series.RemoveAt(0);
+            
+            var seriesP1 = new PieSeries
+            {
+                StrokeThickness = 2.0,
+                InsideLabelPosition = 0.8,
+                AngleSpan = 360,
+                StartAngle = 0,
+                InsideLabelColor = OxyColors.Azure,
+                TextColor = OxyColors.AliceBlue,
+                Stroke = OxyColors.Gray
+            };
+
+            if (emo != null)
+            {
+
+                seriesP1.Slices.Add(new PieSlice("Anger", emo.scores.anger) { IsExploded = true, Fill = OxyColors.Red });
+                seriesP1.Slices.Add(new PieSlice("Contempt", emo.scores.contempt) { IsExploded = true, Fill = OxyColors.MediumPurple });
+                seriesP1.Slices.Add(new PieSlice("Disgust", emo.scores.disgust) { IsExploded = true, Fill = OxyColors.Yellow });
+                seriesP1.Slices.Add(new PieSlice("Fear", emo.scores.fear) { IsExploded = true, Fill = OxyColors.Purple });
+                seriesP1.Slices.Add(new PieSlice("Happiness", emo.scores.happiness) { IsExploded = true, Fill = OxyColors.LightGreen });
+                seriesP1.Slices.Add(new PieSlice("Neutral", emo.scores.neutral) { IsExploded = true, Fill = OxyColors.SandyBrown });
+                seriesP1.Slices.Add(new PieSlice("Sadness", emo.scores.sadness) { IsExploded = true, Fill = OxyColors.DimGray });
+                seriesP1.Slices.Add(new PieSlice("Surprise", emo.scores.surprise) { IsExploded = true, Fill = OxyColors.Orange });
+            }
+            else
+            {
+                seriesP1.Slices.Add(new PieSlice("No Data Available", 1) { IsExploded = false, Fill = OxyColors.Gray, });
+            }
+
+            MyModel.Series.Add(seriesP1);
+            MyModel.InvalidatePlot(true);
         }
     }
 
